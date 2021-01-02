916a0c351cf6f5c9d0e57b0d1bdfbaa3e5c57588
shaysugg <metrosug@gmail.com>
2021-01-02 11:29:40 +0330

    Use IndexSelectionManager + Make height of newComicGroup smaller

--- wutComicReader/ViewControllers/Library/Extensions/Library+CollectionView.swift
+++ wutComicReader/ViewControllers/Library/Extensions/Library+CollectionView.swift
@@ -14,13 +14,13 @@ extension LibraryVC : UICollectionViewDelegate , UICollectionViewDataSource , UI
     
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return fetchResultController.sections?[section].objects?.count ?? 0
+        fetchResultController.sections?[section].objects?.count ?? 0
     }
     
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BookCell", for: indexPath) as! LibraryCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectioViewIDs.comicCell.id, for: indexPath) as! LibraryCell
         cell.isInEditingMode = editingMode
         cell.book = fetchResultController.object(at: indexPath)
         return cell
@@ -28,18 +28,16 @@ extension LibraryVC : UICollectionViewDelegate , UICollectionViewDataSource , UI
     
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return fetchResultController.sections?.count ?? 0
+        fetchResultController.sections?.count ?? 0
     }
     
     
     
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         
-        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "BookHeader", for: indexPath) as! LibraryReusableView
-        let firstSectionComic = fetchResultController.sections?[indexPath.section].objects?.first as? Comic
-        header.headerLabel.text = firstSectionComic?.groupName
-        header.isEditing = editingMode
-        header.indexSet = indexPath.section
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: CollectioViewIDs.comicGroupHeader.id, for: indexPath) as! LibraryReusableView
+        
+        header.headerLabel.text = fetchResultController.sections?[indexPath.section].name
         return header
     }
     
@@ -49,23 +47,27 @@ extension LibraryVC : UICollectionViewDelegate , UICollectionViewDataSource , UI
         return collectionViewCellSize
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        let comic = fetchResultController.sections?[section].objects?.first as! Comic
+        let isGroupForNewComics = comic.ofComicGroup!.isForNewComics
+        
+        if isGroupForNewComics {
+            return CGSize(width: collectionView.bounds.width, height: 10)
+        }else {
+            return CGSize(width: collectionView.bounds.width, height: 50)
+        }
+        
+    }
+    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
-        
         
         let selectedComic = fetchResultController.object(at: indexPath)
 
-
         if editingMode {
-            if !selectedComics.contains(selectedComic) {
-                selectedComics.append(selectedComic)
-                selectedComicsIndexPaths.append(indexPath)
-
-            }
-        }else{
-//
+            indexSelectionManager.insert(indexPath)
             
-//
+        }else{
+
             collectionView.selectItem(at: nil, animated: false, scrollPosition: [])
             let readerVC = storyboard?.instantiateViewController(withIdentifier: "bookReader") as! BookReaderVC
             readerVC.comic = selectedComic
@@ -91,62 +93,19 @@ extension LibraryVC : UICollectionViewDelegate , UICollectionViewDataSource , UI
                     })
                 }
             }
-//
-            
-            
-           
-            
+ 
         }
     }
     
     func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
         
         if editingMode{
-            
-            let deSelectedComic = fetchResultController.object(at: indexPath) as! Comic
-            let deSelectedIndex = selectedComicsIndexPaths.firstIndex(of: indexPath)
-            
-            if selectedComics.contains(deSelectedComic), let _ = deSelectedIndex {
-                guard let comic = selectedComics.firstIndex(of: deSelectedComic) else { return }
-                selectedComics.remove(at: comic)
-                selectedComicsIndexPaths.remove(at: deSelectedIndex!)
-            }
+            indexSelectionManager.remove(indexPath)
         }
     }
     
     //MARK:- Cell Animations
     
-    fileprivate func addOveralyView(withFrame frame:CGRect, complition: @escaping ()->()) {
-        
-        bookCollectionView.addSubview(cellFullSizeView)
-        cellFullSizeView.frame = CGRect(x: frame.minX, y: frame.minY, width: collectionViewCellSize.width , height: collectionViewCellSize.height)
-        
-        cellFullSizeConstraint = [
-            cellFullSizeView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor,constant: 0),
-             cellFullSizeView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 0),
-             cellFullSizeView.widthAnchor.constraint(equalToConstant: bookCollectionView.bounds.width),
-             cellFullSizeView.heightAnchor.constraint(equalToConstant: bookCollectionView.bounds.height)
-        ]
-        
-        NSLayoutConstraint.activate(cellFullSizeConstraint)
-        
-        cellFullSizeView.layer.cornerRadius = 4
-        
-        
-        UIView.animate(withDuration: 1, delay: 0, options: .curveEaseIn, animations: {
-            self.bookCollectionView.layoutIfNeeded()
-            self.cellFullSizeView.layer.cornerRadius = 0
-        }) { (_) in
-            complition()
-        }
-        
-    }
-    
-    fileprivate func  removeCellFullSizeImage() {
-        cellFullSizeView.removeFromSuperview()
-        NSLayoutConstraint.deactivate(cellFullSizeConstraint)
-        cellFullSizeConstraint.removeAll()
-    }
     
     fileprivate func addLoadingView() {
         
