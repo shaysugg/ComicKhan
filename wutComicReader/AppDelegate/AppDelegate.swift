8f28eb96fc390ea59a443ed5b44b06d82670019c
shaysugg <metrosug@gmail.com>
2021-01-05 16:20:10 +0330

    Register ArrayOfStringTransformer

--- wutComicReader/AppDelegate/AppDelegate.swift
+++ wutComicReader/AppDelegate/AppDelegate.swift
@@ -77,6 +77,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
          */
+        ArrayOfStringsTransformer.register()
+        
         let container = NSPersistentContainer(name: "coredata")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
