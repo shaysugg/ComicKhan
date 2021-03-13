5dd191d6e1a5b8d69d902616c02add8dc2607d0f
shaysugg <metrosug@gmail.com>
2021-03-13 12:03:07 +0330

    Do the creation of app cores in Cores

---
+++ wutComicReader/Cores/Cores.swift
@@ -0,0 +1,28 @@
+//
+//  CoresFactory.swift
+//  wutComicReader
+//
+//  Created by Sha Yan on 3/13/21.
+//  Copyright © 2021 wutup. All rights reserved.
+//
+
+import UIKit
+
+final class Cores {
+    static let main = Cores()
+    
+    let extractor: ComicExteractor
+    let dataService: DataService
+    let appfileManager: AppFileManager
+    
+    private init() {
+        extractor = ComicExteractor(userDirectory: URL.userDiractory,
+                                          comicDirectory: URL.comicDiractory)
+        
+        dataService = DataService(managedContext: (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext)
+        
+        appfileManager = AppFileManager(dataService: dataService,
+                                        userDirectory: URL.userDiractory,
+                                        comicDirectory: URL.comicDiractory)
+    }
+}
